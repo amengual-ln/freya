d6d7b5af2b2d8149d78df830cd357cec5501fd80
Lautaro Amengual <amengual.ln@gmail.com>
2021-03-30 01:13:34 -0300

    lazy import pages on route match

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
 import MainLayout from "./layouts/MainLayout";
-import Home from "./pages/Home";
-import Tareas from "./pages/Tareas";
-import Portafolios from "./pages/Portafolios";
-import Proyectos from "./pages/Proyectos";
-import Notas from "./pages/Notas";
+
+const Home = lazy(() => import("./pages/Home"));
+const Tareas = lazy(() => import("./pages/Tareas"));
+const Portafolios = lazy(() => import("./pages/Portafolios"));
+const Proyectos = lazy(() => import("./pages/Proyectos"));
+const Notas = lazy(() => import("./pages/Notas"));
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
-        <MainLayout>
-          <Route exact path="/" component={Home} />
-          <Route path="/tareas" component={Tareas} />
-          <Route path="/portafolios" component={Portafolios} />
-          <Route path="/proyectos" component={Proyectos} />
-          <Route path="/notas" component={Notas} />
-        </MainLayout>
+        <Suspense fallback={<h1>Loading</h1>}>
+          <MainLayout>
+            <Route exact path="/" component={Home} />
+            <Route path="/tareas" component={Tareas} />
+            <Route path="/portafolios" component={Portafolios} />
+            <Route path="/proyectos" component={Proyectos} />
+            <Route path="/notas" component={Notas} />
+          </MainLayout>
+        </Suspense>
       </Router>
     </Provider>
   );
