f4990d337cd06fc2ba9fadcd1c5e8cad81a76bbc
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-03 18:08:01 -0300

    Redux selector for projects in specific briefcase

--- src/store/selectors/projects.js
+++ src/store/selectors/projects.js
@@ -2,3 +2,6 @@ export const getProjectsState = (store) => store.projects;
 
 export const getProjects = (store) =>
   getProjectsState(store) ? getProjectsState(store) : [];
+
+export const getBriefcaseProjects = (store, briefcaseId) =>
+  getProjectsState(store)?.filter((project) => project.briefcaseId === briefcaseId);
