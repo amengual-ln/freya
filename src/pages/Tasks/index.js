1354184336f70b5f07cde10668d41410582438f4
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-03 18:35:18 -0300

    Added list of tasks in the tasks page

--- src/pages/Tasks/index.js
+++ src/pages/Tasks/index.js
@@ -1,3 +1,17 @@
+import { useSelector } from "react-redux";
+import { getTasks } from "../../store/selectors/tasks";
+
+import List from "../../components/List"
+import ListItem from "../../components/ListItem"
+
 export default function Tasks () {
-  return <h1>Tareas!</h1>
+  const tasks = useSelector(state => (getTasks(state)))
+
+  return (
+    <List title="Tareas!">
+      {tasks.map((task) => (
+        <ListItem item={task} key={task.id} />
+      ))}
+    </List>
+  )
 }
