d02ea4d57d5a28889e1e71519994e0219ee0a3be
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-03 14:47:44 -0300

    Specific briefcase redux selector

--- src/store/selectors/briefcases.js
+++ src/store/selectors/briefcases.js
@@ -2,3 +2,6 @@ export const getBriefcasesState = (store) => store.briefcases;
 
 export const getBriefcases = (store) =>
   getBriefcasesState(store) ? getBriefcasesState(store) : [];
+
+export const getBriefcase = (store, id) =>
+  getBriefcasesState(store)?.find(briefcase => briefcase.id === id)
