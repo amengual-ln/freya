b0da77e9c91500567357faa1f486763a801522e4
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-03 18:11:42 -0300

    Filter keys to show in ListItem

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,20 +1,30 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Item = styled.tr`
-  border-bottom: 1px solid rgba(0,0,0,.2);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
   td {
     padding: 10px;
   }
-`
+`;
 
-export default function ListItem ({item}) {
+export default function ListItem({ item, show }) {
+  let propsToShow = Object.values(item);
+  if (show) {
+    propsToShow = Object.keys(item)
+      .filter((key) => show.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = item[key];
+        return obj;
+      }, {});
+  }
+
+  const propValues = Object.values(propsToShow).map((prop) => prop);
+
   return (
     <Item>
-      <td>{item.status}</td>
-      <td>{item.title}</td>
-      <td>{item.assigne}</td>
-      <td>{item.project}</td>
-      <td>{item.dueDate}</td>
+      {propValues.map((prop, index) => (
+        <td key={`${index}${prop}`}>{prop}</td>
+      ))}
     </Item>
-  )
+  );
 }
