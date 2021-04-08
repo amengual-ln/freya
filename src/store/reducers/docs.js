107edd7779bbe4f558a799d232d3f5ac27689231
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-08 00:53:24 -0300

    Update docs state

--- src/store/reducers/docs.js
+++ src/store/reducers/docs.js
@@ -1,11 +1,13 @@
 const initialState = [
   {
     id: "1",
-    title: "Weekly review 3/4",
+    title: "<h2>Weekly review 3/4</h2>",
+    content: "<p>Contenido del resumen semanal a día 3 de abril de 2021</p>"
   },
   {
     id: "2",
-    title: "Curso resolución de problemas",
+    title: "<h2>Curso resolución de problemas</h2>",
+    content: "<p>Contenido de los apuntes tomados sobre el curso de resolución de problemas de Crehana</p>"
   },
 ];
 
