6b607ccdcc5af51c7eeb167906594d6ffeb985ad
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-24 10:06:24 -0300

    Now including created an updated datetimes in Docs

--- src/store/reducers/docs.js
+++ src/store/reducers/docs.js
@@ -7,7 +7,7 @@ export default function state(state = initialState, action) {
   if (action.type === "@docs/GET_DOCS") {
     db.collection("docs").onSnapshot((snapshot) => {
       snapshot.docs.forEach((doc) => {
-        if(!state.find(previousDoc => previousDoc.id === doc.id)) {
+        if (!state.find((previousDoc) => previousDoc.id === doc.id)) {
           state.push({
             id: doc.id,
             ...doc.data(),
@@ -17,7 +17,8 @@ export default function state(state = initialState, action) {
     });
   }
   if (action.type === "@docs/SAVE_CHANGES") {
-    const values = action.payload;
+    const now = new Date();
+    const values = { ...action.payload, updatedAt: now };
     db.collection("docs").doc(values.id).update(values);
     state = state.map((doc) => {
       if (doc.id === values.id) {
@@ -25,6 +26,7 @@ export default function state(state = initialState, action) {
           id: values.id ? values.id : doc.id,
           title: values.title ? values.title : doc.title,
           content: values.content ? values.content : doc.content,
+          updatedAt: now,
         };
       }
       return doc;
@@ -34,10 +36,12 @@ export default function state(state = initialState, action) {
 }
 
 export const SaveNewDoc = () => async (dispatch, getState) => {
+  const now = new Date();
   const values = {
     title: "<h1>Nuevo documento</h1>",
     content: "<p>Contenido del documento...</p><br/><br/><br/>",
-    createdAt: new Date(),
+    createdAt: now,
+    updatedAt: now,
   };
   const res = await db.collection("docs").add(values);
   db.collection("docs").doc(res.id).update({ id: res.id });
