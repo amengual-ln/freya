7ed6de9eff0b975786d9424debf1ca23acbfd039
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-24 08:46:27 -0300

    Small router refactor to use custom history

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { lazy, Suspense, useEffect } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
+import { Route } from "react-router-dom";
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+
 import { useDispatch } from "react-redux";
 
 import MainLayout from "./layouts/MainLayout";
@@ -12,26 +15,27 @@ const Proyectos = lazy(() => import("./pages/Projects"));
 const Docs = lazy(() => import("./pages/Docs"));
 const Doc = lazy(() => import("./pages/Doc"));
 
+export const history = createBrowserHistory();
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch({ type: '@docs/GET_DOCS' });
-  })
+    dispatch({ type: "@docs/GET_DOCS" });
+  });
 
   return (
-    <Router>
-        <MainLayout>
-          <Suspense fallback={<div></div>}>
-            <Route exact path="/" component={Home} />
-            <Route path="/tasks" component={Tareas} />
-            <Route path="/briefcases" component={Portafolios} />
-            <Route path="/briefcase/:id" component={Portafolio} />
-            <Route path="/projects" component={Proyectos} />
-            <Route path="/docs" component={Docs} />
-            <Route path="/doc/:id" component={Doc} />
-          </Suspense>
-        </MainLayout>
+    <Router history={history}>
+      <MainLayout>
+        <Suspense fallback={<div></div>}>
+          <Route exact path="/" component={Home} />
+          <Route path="/tasks" component={Tareas} />
+          <Route path="/briefcases" component={Portafolios} />
+          <Route path="/briefcase/:id" component={Portafolio} />
+          <Route path="/projects" component={Proyectos} />
+          <Route path="/docs" component={Docs} />
+          <Route path="/doc/:id" component={Doc} />
+        </Suspense>
+      </MainLayout>
     </Router>
   );
 }
