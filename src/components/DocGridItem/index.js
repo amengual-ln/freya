c9c504136b5d666de293f7a21fa1394e6d7569f2
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-24 10:46:29 -0300

    Show last updated time in the Doc cards

--- src/components/DocGridItem/index.js
+++ src/components/DocGridItem/index.js
@@ -1,12 +1,17 @@
-import { Link } from 'react-router-dom'
-import styled from "styled-components"
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import styled from "styled-components";
+
+var relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+require('dayjs/locale/es')
 
 const Doc = styled.div`
   color: #202020;
   background: #fff;
   border-radius: 5px;
   box-shadow: 0 2px 2px rgb(0 0 0 / 15%);
-  transition: .2s;
+  transition: 0.2s;
   text-align: center;
   &:hover {
     box-shadow: 0 4px 4px rgb(0 0 0 / 15%);
@@ -18,15 +23,18 @@ const Doc = styled.div`
     color: inherit;
     text-decoration: none;
   }
-`
+`;
 
-export default function DocGridItem ({ doc, url }) {
-  const title = doc.title.replace(/(<([^>]+)>)/ig, '');
+export default function DocGridItem({ doc, url }) {
+  const title = doc.title.replace(/(<([^>]+)>)/gi, "");
+  const updatedAt = dayjs().locale('es').to(dayjs(doc.updatedAt?.toDate(), true))
+  console.log(doc.updatedAt?.toDate());
   return (
     <Doc>
       <Link to={`/${url}/${doc.id}`}>
-        <h4>{title}</h4>
+        <h4>{ title }</h4>
+        <pre>{ updatedAt }</pre>
       </Link>
     </Doc>
-  )
+  );
 }
