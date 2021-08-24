64faab34daded0d904a8e931d6f091d97696e2c1
Lautaro Amengual <amengual.ln@gmail.com>
2021-08-24 20:16:47 -0300

    Lint and fix

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,21 +1,27 @@
-import { useSelector } from "react-redux";
-import { getLinks } from "../../store/selectors/links";
+import { useSelector } from 'react-redux'
+import { getLinks } from '../../store/selectors/links'
 
-import Grid from "../../components/Grid";
-import Card from "../../components/Card";
+import Grid from '../../components/Grid'
+import Card from '../../components/Card'
 
 export default function Links() {
-  const links = useSelector((state) => getLinks(state));
+	const links = useSelector((state) => getLinks(state))
 
-  return (
-    <>
-      <Grid title="Links" itemWidth="270px">
-        {links.map((link) => (
-          <Card href={link.url} deletable collection="links" id={link.id} key={link.id}>
-            <h4>{link.title}</h4>
-          </Card>
-        ))}
-      </Grid>
-    </>
-  );
+	return (
+		<>
+			<Grid title="Links" itemWidth="270px">
+				{links.map((link) => (
+					<Card
+						href={link.url}
+						deletable
+						collection="links"
+						id={link.id}
+						key={link.id}
+					>
+						<h4>{link.title}</h4>
+					</Card>
+				))}
+			</Grid>
+		</>
+	)
 }
