570d82fd2e3976f6d871dffbc205a25aaaa11cde
Lautaro Amengual <amengual.ln@gmail.com>
2021-08-24 18:36:13 -0300

    Fix typo

--- src/pages/Tasks/index.js
+++ src/pages/Tasks/index.js
@@ -5,12 +5,12 @@ import List from "../../components/List"
 import ListItem from "../../components/ListItem"
 
 export default function Tasks () {
-  const tasks = useSelector(state => (getTasks(state)))
+  const tasks = useSelector(state => getTasks(state))
 
   return (
     <List title="Tareas!">
       {tasks.map((task) => (
-        <ListItem item={task} show={["title","assigne", "project", "status"]} key={task.id} />
+        <ListItem item={task} show={["title","assignee", "project", "status"]} key={task.id} />
       ))}
     </List>
   )
