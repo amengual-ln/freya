aaee06906dbda0a26c724f7cdc581989ca5b2280
Lautaro Amengual <amengual.ln@gmail.com>
2021-09-08 18:49:21 -0300

    Docs page refactor: replaced the DocGridItem component with just Cards. Replaced the NewResourceButton with the new Button component.

--- src/pages/Docs/index.js
+++ src/pages/Docs/index.js
@@ -1,22 +1,51 @@
-import { useSelector } from "react-redux";
-import { getDocs } from "../../store/selectors/docs";
+import { useSelector, useDispatch } from 'react-redux'
+import { getDocs } from '../../store/selectors/docs'
+import dayjs from 'dayjs'
 
-import { Grid } from "../../components/Grid";
-import DocGridItem from "../../components/DocGridItem";
-import NewResourceButton from "../../components/NewResourceButton";
+import { Grid } from '../../components/Grid'
+import { Button } from '../../components/atoms/Button'
+import Card from '../../components/Card'
+import { createResource } from '../../store/reducers/resources'
 
-export default function Docs () {
-  const docs = useSelector(state => getDocs(state))
+var relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+require('dayjs/locale/es')
 
-  return (
-    <section>
-      <h2>Docs</h2>
-      <Grid>
-        <NewResourceButton collection='docs' />
-        {docs.map((doc) => (
-          <DocGridItem doc={doc} url="doc" key={doc.id} />
-        ))}
-      </Grid>
-    </section>
-  )
+export default function Docs() {
+	const dispatch = useDispatch()
+	const docs = useSelector((state) => getDocs(state))
+
+	const getFormattedDate = (date) => {
+		date = date.toDate ? date.toDate() : dayjs(date).toDate()
+		return dayjs().locale('es').to(dayjs(date))
+	}
+
+	return (
+		<section>
+			<h2>Docs</h2>
+			<Grid>
+				<Button
+					circle
+					handleClick={() => dispatch(createResource('docs'))}
+				>
+					Nuevo +
+				</Button>
+				{docs.map((doc) => (
+					<Card
+						to={`/doc/${doc.id}`}
+						deletable
+						collection="docs"
+						id={doc.id}
+						key={doc.id}
+					>
+						<div className="my-auto">
+							<h4>{doc.title.replace(/(<([^>]+)>)/gi, '')}</h4>
+							<br />
+							<pre>{getFormattedDate(doc.updatedAt)}</pre>
+						</div>
+					</Card>
+				))}
+			</Grid>
+		</section>
+	)
 }
