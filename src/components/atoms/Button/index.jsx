041799ffefa731fba049f1bb8e1901de4c43c2b5
Lautaro Amengual <amengual.ln@gmail.com>
2021-09-08 18:47:02 -0300

    Implemented a basic Button component with some variants.

---
+++ src/components/atoms/Button/index.jsx
@@ -0,0 +1,14 @@
+const primaryStyles = 'rounded mt-2.5 ml-2 py-1.5 px-3 text-blue-900 bg-blue-100 hover:bg-blue-200'
+const circleStyles = 'bg-gray-200 w-24 h-24 m-auto rounded-full'
+const defaultStyles = 'rounded mt-2.5 py-1.5 px-3 hover:bg-gray-200'
+
+export const Button = ({ children, handleClick, circle, primary }) => {
+	return (
+		<button
+			onClick={() => handleClick()}
+			className={ circle ? circleStyles : primary ? primaryStyles : defaultStyles }
+		>
+			{children}
+		</button>
+	)
+}
