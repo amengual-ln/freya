cec1c4e54e034f526805789d646a6fee7d0010a5
Lautaro Amengual <amengual.ln@gmail.com>
2022-04-09 21:37:33 -0300

    Implement briefcase creation form

---
+++ src/components/AddBriefcaseForm/index.jsx
@@ -0,0 +1,69 @@
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+
+import { Button } from '../../components/atoms/Button'
+import { createResource } from '../../store/reducers/resources'
+
+export const AddBriefcaseForm = ({ handleClose }) => {
+	const dispatch = useDispatch()
+	const { register, handleSubmit, reset } = useForm()
+
+	const onSubmit = async (data) => {
+		dispatch(
+			createResource('briefcases', {
+				...data,
+			})
+		)
+		handleClose()
+		reset()
+	}
+
+	const onCancel = (event) => {
+		event.preventDefault()
+		handleClose()
+		reset()
+	}
+
+	return (
+		<>
+			<h2 className="font-medium mb-3.5">Nuevo portafolios</h2>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<label htmlFor="title" className="text-sm">
+					Nombre
+				</label>
+				<input
+					{...register('name')}
+					id="title"
+					autoComplete="off"
+					className="w-full bg-gray-100 rounded mb-3.5 p-1.5"
+				/>
+				<label htmlFor="title" className="text-sm">
+					Color
+				</label>
+				<input
+					{...register('color')}
+					id="title"
+					autoComplete="off"
+					className="w-full bg-gray-100 rounded mb-3.5 p-1.5"
+				/>
+				<label htmlFor="title" className="text-sm">
+					Descripción
+				</label>
+				<input
+					{...register('description')}
+					id="description"
+					autoComplete="off"
+					className="w-full bg-gray-100 rounded mb-3.5 p-1.5"
+				/>
+				<div className="flex justify-end">
+					<Button handleClick={(event) => onCancel(event)}>
+						Cancelar
+					</Button>
+					<Button primary>
+						Crear
+					</Button>
+				</div>
+			</form>
+		</>
+	)
+}
--- src/pages/Briefcases/index.js
+++ src/pages/Briefcases/index.js
@@ -3,23 +3,38 @@ import { getBriefcases } from '../../store/selectors/briefcases'
 
 import { Grid } from '../../components/Grid'
 import GridItem from '../../components/GridItem'
+import { Button } from '../../components/atoms/Button'
+import { useModal } from '../../hooks/useModal'
+import { Modal } from '../../components/Modal'
+import { AddBriefcaseForm } from '../../components/AddBriefcaseForm'
 
 export default function Briefcases() {
 	const briefcases = useSelector((state) => getBriefcases(state))
+	const { isOpen, toggle } = useModal()
+
+	const handleNewBriefcase = () => toggle()
 
 	return (
-		<section>
-			<h2>Portafolios</h2>
-			<Grid>
-				{briefcases.map((briefcase) => (
-					<GridItem
-						item={briefcase}
-						url="briefcase"
-						filled
-						key={briefcase.id}
-					/>
-				))}
-			</Grid>
-		</section>
+		<>
+			<section>
+				<h2>Portafolios</h2>
+				<Grid>
+					<Button card handleClick={() => handleNewBriefcase()}>
+						Nuevo +
+					</Button>
+					{briefcases.map((briefcase) => (
+						<GridItem
+							item={briefcase}
+							url="briefcase"
+							filled
+							key={briefcase.id}
+						/>
+					))}
+				</Grid>
+			</section>
+			<Modal isOpen={isOpen} hide={toggle}>
+				<AddBriefcaseForm handleClose={toggle} />
+			</Modal>
+		</>
 	)
 }
