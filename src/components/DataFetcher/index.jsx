faea39755b33b155cff0708ba12821f7f85e90e9
Lautaro Amengual <amengual.ln@gmail.com>
2022-04-29 20:36:04 -0300

    Run on port 4242 remove side effect Remove side effect from data fetcher

--- src/components/DataFetcher/index.jsx
+++ src/components/DataFetcher/index.jsx
@@ -1,16 +1,14 @@
-import { useEffect } from 'react'
 import { useDispatch } from "react-redux";
 import { fetchResource } from "../../store/reducers/resources";
 
 export const DataFetcher = ({ children }) => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchResource('tasks'))
-    dispatch(fetchResource('briefcases'))
-    dispatch(fetchResource('projects'))
-    dispatch(fetchResource('docs'))
-    dispatch(fetchResource('links'))
-  }, [dispatch]);
+
+  dispatch(fetchResource('tasks'))
+  dispatch(fetchResource('briefcases'))
+  dispatch(fetchResource('projects'))
+  dispatch(fetchResource('docs'))
+  dispatch(fetchResource('links'))
 
   return (
     <>
