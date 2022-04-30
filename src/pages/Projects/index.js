16b41c20a3f6212d222db6d870d56ffedeb5fb5b
Lautaro Amengual <amengual.ln@gmail.com>
2022-04-30 15:51:14 -0300

    Don't show inactive projects in home Split projects by active/inactive

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux'
-import { getProjects } from '../../store/selectors/projects'
+import { getHomeProjects } from '../../store/selectors/projects'
+import { getInactiveProjects } from '../../store/selectors/projects'
 
 import { Button } from '../../components/atoms/Button'
 import { Grid } from '../../components/Grid'
@@ -8,8 +9,9 @@ import { Modal } from '../../components/Modal'
 import { useModal } from '../../hooks/useModal'
 import { AddProjectForm } from '../../components/AddProjectForm'
 
-export default function Projects() {
-	const projects = useSelector((state) => getProjects(state))
+export default function Projects({ home = false }) {
+	const projects = useSelector((state) => getHomeProjects(state))
+	const inactiveProjects = useSelector((state) => getInactiveProjects(state))
 	const { isOpen, toggle } = useModal()
 
 	const handleNewProject = () => toggle()
@@ -26,6 +28,16 @@ export default function Projects() {
 						<GridItem item={project} url="project" filled key={project.id} />
 					))}
 				</Grid>
+				{!home &&
+					<>
+						<h4 className="font-medium text-gray-500 mt-6 -mb-1">INACTIVOS</h4>
+						<Grid>
+							{inactiveProjects.map((project) => (
+								<GridItem item={project} url="project" filled key={project.id} />
+							))}
+						</Grid>
+					</>
+				}
 			</section>
 			<Modal isOpen={isOpen} hide={toggle}>
 				<AddProjectForm handleClose={toggle} />
--- src/store/selectors/projects.js
+++ src/store/selectors/projects.js
@@ -3,6 +3,15 @@ export const getProjectsState = (store) => store.projects
 export const getProjects = (store) =>
 	getProjectsState(store) ? getProjectsState(store) : []
 
+export const getActiveProjects = (store) =>
+	getProjectsState(store).filter((project) => project.active)
+
+export const getInactiveProjects = (store) =>
+	getProjectsState(store).filter((project) => !project.active)
+
+export const getHomeProjects = (store) =>
+	getActiveProjects(store).slice(0, 6)
+
 export const getProject = (store, id) =>
 	getProjectsState(store)?.find((project) => project.id === id)
 
