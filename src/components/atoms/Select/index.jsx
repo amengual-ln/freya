fb8fb49e8903e1a0918eb1052bab412b6d48cdc5
Lautaro Amengual <amengual.ln@gmail.com>
2022-09-08 18:48:54 -0300

    Custom select component Based on HeadlessUI

---
+++ src/components/atoms/Select/index.jsx
@@ -0,0 +1,42 @@
+import { useState, Fragment } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+
+export const Select = ({options, initial}) => {
+    const [selected, setSelected] = useState(initial)
+
+    return (
+        <Listbox value={selected} onChange={setSelected}>
+            <div className='relative mt-1'>
+                <Listbox.Button className='relative w-full rounded bg-gray-100 p-1.5 text-left focus:outline-none'>
+                    <span className='block truncate'>{selected?.name || 'Ninguno'}</span>
+                    <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+                        ˅
+                    </span>
+                </Listbox.Button>
+                <Transition
+                    as={Fragment}
+                    enter='transition ease-in duration-100'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='transition ease-in duration-100'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                >
+                    <Listbox.Options className='absolute mt-1 max-h-60 w-full rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-sm z-30'>
+                        {options.map((option) => (
+                            <Listbox.Option
+                                key={option.id}
+                                className='relative select-none cursor-pointer py-2 pl-4 pr-4 hover:text-blue-500'
+                                value={option.id}
+                            >
+                                <span className='block truncate'>
+                                    {option.name}
+                                </span>
+                            </Listbox.Option>
+                        ))}
+                    </Listbox.Options>
+                </Transition>
+            </div>
+        </Listbox>
+    )
+}
