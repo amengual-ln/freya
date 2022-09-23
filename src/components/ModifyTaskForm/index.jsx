facd55f5da3766033b7289cabe84723f252705bf
Lautaro Amengual <amengual.ln@gmail.com>
2022-09-22 22:00:29 -0300

    Replace Listbox with new Select component

--- src/components/ModifyTaskForm/index.jsx
+++ src/components/ModifyTaskForm/index.jsx
@@ -16,6 +16,7 @@ const status = [
 export const ModifyTaskForm = ({ task, handleClose }) => {
     const dispatch = useDispatch()
     const projects = useSelector(state => getActiveProjects(state))
+    const [selectedProject, setSelectedProject] = useState(task.project)
     const [selectedStatus, setSelectedStatus] = useState(task.status)
     const { register, handleSubmit, reset } = useForm({
         defaultValues: {
@@ -26,15 +27,20 @@ export const ModifyTaskForm = ({ task, handleClose }) => {
         }
     })
 
-    const handleStatusChange = () => {
-        setSelectedStatus(task.status)
+    const handleProjectChange = (newProject) => {
+        setSelectedProject(newProject)
+    }
+
+    const handleStatusChange = (newStatus) => {
+        setSelectedStatus(newStatus)
     }
 
     const onSubmit = async (data) => {
         dispatch(
             modifyResource('tasks', task.id, {
                 ...data,
-                status: selectedStatus
+                status: selectedStatus,
+                projectId: selectedProject.id
             })
         )
         handleClose()
@@ -62,55 +68,12 @@ export const ModifyTaskForm = ({ task, handleClose }) => {
                 />
                 <label htmlFor="projectId" className="text-sm">
                     Proyecto
+                    <Select options={projects} initial={task.project} handleChange={handleProjectChange} />
                 </label>
-                {/* <input
-                    {...register('projectId')}
-                    id="projectId"
-                    autoComplete="off"
-                    className="w-full bg-gray-100 rounded mb-3.5 p-1.5"
-                /> */}
-                <Select options={projects} initial={task.project} />
                 <label htmlFor="status" className="text-sm">
                     Estado
+                    <Select options={status} initial={task.status} handleChange={handleStatusChange} />
                 </label>
-                <Select options={status} initial={task.status} />
-                {/* <Listbox value={selectedStatus} onChange={setSelectedStatus}>
-                    <div className="relative mt-1">
-                        <Listbox.Button className="relative w-full rounded bg-gray-100 p-1.5 text-left focus:outline-none">
-                            <span className="block truncate">{selectedStatus}</span>
-                            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                                ˅
-                            </span>
-                        </Listbox.Button>
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-in duration-100"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                            leave="transition ease-in duration-100"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                        >
-                            <Listbox.Options className="absolute mt-1 max-h-60 w-full rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-sm">
-                                {status.map((status) => (
-                                    <Listbox.Option
-                                        key={status}
-                                        className={`relative select-none cursor-pointer py-2 pl-4 pr-4 hover:text-blue-500`}
-                                        value={status}
-                                    >
-
-                                        <span
-                                            className={`block truncate ${selectedStatus === status ? 'font-medium' : 'font-normal'
-                                                }`}
-                                        >
-                                            {status}
-                                        </span>
-                                    </Listbox.Option>
-                                ))}
-                            </Listbox.Options>
-                        </Transition>
-                    </div>
-                </Listbox> */}
                 <div className="flex justify-end mt-2.5">
                     <Button handleClick={(event) => onCancel(event)}>
                         Cancelar
